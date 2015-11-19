36cccf11d0deffd7b1e60ad2bd959c3ab8886190
Rob O'Quinn <rob.oquinn@gmail.com>
2015-11-18 21:31:58 -0400

    Spawning multiple tanks, of different colours and at different positions

---
+++ Assets/Scripts/Tank.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tank : MonoBehaviour
+{
+    public GameObject TankBody;
+    public GameObject TankHood;
+    public GameObject TankGun;
+
+    MeshRenderer tankBodyRenderer;
+    MeshRenderer tankHoodRenderer;
+    MeshRenderer tankGunRenderer;
+
+    void Awake()
+    {
+        tankBodyRenderer = TankBody.GetComponent<MeshRenderer>();
+        tankHoodRenderer = TankHood.GetComponent<MeshRenderer>();
+        tankGunRenderer = TankGun.GetComponent<MeshRenderer>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetColor(Color color)
+    {
+        tankBodyRenderer.material.color = color;
+        tankHoodRenderer.material.color = color;
+        tankGunRenderer.material.color = color;
+    }
+}
---
+++ Assets/Scripts/TankSpawner.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TankSpawner : MonoBehaviour
+{
+    public int NumberOfTanks;
+    public GameObject TankPrefab;
+
+    public Color[] TankColors;
+
+    // Use this for initialization
+    void Start()
+    {
+        for (int i = 0; i < NumberOfTanks; ++i)
+        {
+            GameObject newTankObject = Instantiate(TankPrefab);
+            newTankObject.name = "Tank" + i.ToString("D2");
+            Vector3 position = new Vector3(Random.Range(-10.0f, 10.0f), 0.0f, Random.Range(-10.0f, 10.0f));
+            newTankObject.transform.position = position;
+            float yRotation = Random.Range(0.0f, 360.0f);
+            Quaternion rotation = Quaternion.AngleAxis(yRotation, Vector3.up);
+            newTankObject.transform.rotation = rotation;
+
+            Tank newTank = newTankObject.GetComponent<Tank>();
+
+            newTank.SetColor(TankColors[i % TankColors.Length]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
